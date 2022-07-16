fd4dddcfddf4d3c4c9c2ab3f84f1ac30e2b8397e
aristotleboi <gage24paul@gmail.com>
2022-07-15 20:48:32 -0700

    fixed git error and put todo class in its own module. also got the function to load todos from a list to the dom

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import {loadFromList} from './to-do-dom';
+import toDo from './to-do'
+
 let toDolists = [];
 
 // factory function for list
@@ -7,7 +10,7 @@ const List = (name) => {
     const addToDoToArray = (toDo) => {
         singleList.push(toDo)
     }
-    const seeArray = () => console.log(singleList);
+    const getToDos = () => {return singleList};
     const removeToDO = (arraynumber) => {
         singleList.splice(arraynumber, 1);
     }
@@ -15,21 +18,11 @@ const List = (name) => {
     return {
         getName,
         addToDoToArray,
-        seeArray,
+        getToDos,
         removeToDO
     }
 }
 
-//new to do class constructor
-class toDo {
-    constructor(title, description, dueDate, priority, checked) {
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
-        this.checked = checked;
-    }
-}
 
 function addListToArray(list) {
     toDolists.push(list);
@@ -40,6 +33,5 @@ let toDo1 = new toDo("call the doctor", "call", "07/04/22", 'low', false);
 const list1 = List("appointments");
 
 list1.addToDoToArray(toDo1);
-list1.seeArray();
 
-
+loadFromList(list1.getToDos())
--- src/to-do-dom.js
+++ src/to-do-dom.js
@@ -1,30 +1,30 @@
-const initialLoad = (list, listTitle, toDO, tdTitle, tdDescription, tdDueDate, tdChecked) => {
+
+
+const loadFromList = (list) => {
     const content = document.querySelector('.content');
     const header = document.querySelector('.to-do-lists')
-
-    //inital load of a list
-    listTab = document.createElement('div');
-    listTab.innerText = listTitle
-
-    //initial load of a to-do copied from my library app
-    const card = document.createElement('div');
-    card.classList.add('card');
-    let title = document.createElement('h3');
-    title.innerText = tdTitle;
-    title.setAttribute('class', 'to-do-title')
-    let description = document.createElement('p')
-    description.innerText = tdDescription;
-    description.setAttribute('class', 'to-do-description');
-    let date = document.createElement('p');
-    date.innerText = tdDueDate;
-    date.setAttribute('class', 'to-do-date');
-    let checked = document.createElement('div')
-    checked.innerText = tdChecked;
-    checked.setAttribute('class', 'to-do-checked');
-    card.appendChild(title);
-    card.appendChild(description);
-    card.appendChild(date);
-    card.appendChild(checked);
-    content.appendChild(card);
-
+    let numberOfToDos = list.length;
+    for (let i = 0; i < numberOfToDos; i++) {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        let title = document.createElement('h3');
+        title.innerText = list[i].title;
+        title.setAttribute('class', 'to-do-title')
+        let description = document.createElement('p')
+        description.innerText = list[i].description;
+        description.setAttribute('class', 'to-do-description');
+        let date = document.createElement('p');
+        date.innerText = list[i].dueDate;
+        date.setAttribute('class', 'to-do-date');
+        let checked = document.createElement('div')
+        checked.innerText = list[i].checked;
+        checked.setAttribute('class', 'to-do-checked');
+        card.appendChild(title);
+        card.appendChild(description);
+        card.appendChild(date);
+        card.appendChild(checked);
+        content.appendChild(card);
+    }
 }
+
+export {loadFromList}
